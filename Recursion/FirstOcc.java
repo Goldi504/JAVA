8bcfeadc22302fe0fdb1c4f7fa820dfe7ae61d2f
Goldi504 <goldikumari107@gmail.com>
2025-12-25 19:31:34 +0530

    Updated files

--- Recursion/FirstOcc.java
+++ Recursion/FirstOcc.java
@@ -13,10 +13,28 @@ public class FirstOcc {
     }
     return FirstOccurrence(arr, i+1, key);
 }
+
+
+
+public static int lastOccurrence(int arr[] , int i,int key){
+    if (i == arr.length) {
+        return -1;
+        
+    }
+    int isFound = lastOccurrence(arr, i+1 , key);
+    if(isFound == -1 && arr[i]==key){
+        return i;        
+    }
+    return isFound;
+}
+    
+  
+
+
 public static void main(String[] args) {
-   int arr[] ={2,3,4,5,6,7,8};
+   int arr[] ={2,3,4,5,6,7,5,8};
    int key = 5;
-   System.out.println(key);
+   System.out.println(lastOccurrence(arr, 0 ,5));
 }
 
     
