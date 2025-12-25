fd80aa6d1193793a5a17a69d5959b32d1b0c2cb9
Goldi504 <goldikumari107@gmail.com>
2025-12-25 19:02:46 +0530

    parameter constructor

---
+++ OOPS/parameter.java
@@ -0,0 +1,24 @@
+package OOPS;
+
+public class parameter {
+    public static void main(String[] args) {
+        Student s1= new Student();
+        Student s2 = new Student("afdfd");
+        Student s3 = new Student(123);        
+    }
+}
+     class Student{
+        String name;
+        int roll;
+        Student(){
+            System.out.println("Constructor is called ");
+        }
+        Student(String name){
+            this.name = name;
+        }
+        Student(int roll){
+            this.roll = roll;
+        }
+     }
+    
+
